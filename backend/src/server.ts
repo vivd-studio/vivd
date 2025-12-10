1d6e7e866bf8e6f1d667077501088971eb52317e
felixpahlke <fe.pahlke@gmail.com>
2025-12-10 08:52:47 +0100

    add domain to cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.DOMAIN,
     credentials: true
 }));
 app.use(express.json());
