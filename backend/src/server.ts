adc6b30ab59627cc520f8de296dae164f715ffef
felixpahlke <fe.pahlke@gmail.com>
2025-12-10 08:55:17 +0100

    Revert Domain Cors changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: process.env.DOMAIN,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }));
 app.use(express.json());
