ce22bd0929352dde4153106d7bb9eb540c5340b8
felixpahlke <fe.pahlke@gmail.com>
2025-12-20 03:02:08 +0100

    refactor: remove ExitConfirmationDialog export from preview index

--- frontend/src/components/preview/index.ts
+++ frontend/src/components/preview/index.ts
@@ -4,5 +4,4 @@ export { PreviewToolbar } from "./PreviewToolbar";
 export { PreviewIframe } from "./PreviewIframe";
 export { MobileFrame } from "./MobileFrame";
 export { UnsavedChangesBar } from "./UnsavedChangesBar";
-export { ExitConfirmationDialog } from "./ExitConfirmationDialog";
 export type { DevicePreset } from "./types";
