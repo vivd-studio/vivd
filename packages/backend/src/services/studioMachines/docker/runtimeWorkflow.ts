f55c8e6845cd1fe964204eb90967518f37ea6789
felixpahlke <fe.pahlke@gmail.com>
2026-03-31 11:14:23 +0200

    refactor: remove unused isLikelyRemoteImageReference import from runtimeWorkflow.ts

--- packages/backend/src/services/studioMachines/docker/runtimeWorkflow.ts
+++ packages/backend/src/services/studioMachines/docker/runtimeWorkflow.ts
@@ -28,7 +28,6 @@ import {
   isContainerNameConflictError,
   isContainerNetworkingSetupError,
   isContainerPortConflictError,
-  isLikelyRemoteImageReference,
   isMissingImageError,
   isMissingNativeManifestError,
   isRunningContainer,
